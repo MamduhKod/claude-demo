c73984e32dba66929b806f1788f580064a670653
Mamduh Halawa <mamduhkod@gmail.com>
2025-06-03 15:07:28 +0200

    analysis

---
+++ src/sentiment_analysis.py
@@ -0,0 +1,30 @@
+import anthropic
+import dotenv
+import os
+from SO import structured_output_translation
+import json
+
+# Load environment variables from .env file
+dotenv.load_dotenv()
+
+# Get the API key from the environment variables
+api_key = os.environ.get("ANTHROPIC-API-KEY")
+
+# Create an Anthropic client instance with the API key
+client = anthropic.Anthropic(api_key=api_key)
+
+response = client.messages.create(
+    model="claude-3-opus-20240229",
+    max_tokens=4096,
+    tools=[structured_output_translation],
+    tool_choice={"type": "tool", "name": "translation"},
+    messages=[{"role": "user", "content": "Baby"}],
+)
+
+response
+
+for content in response.content:
+    print(content)
+    if content.type == "tool_use" and content.name == "translation":
+        result_json = json.dumps(content.input, indent=2, ensure_ascii=False)
+        print(result_json)
